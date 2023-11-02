9f67916e1ae2fd5de5823e21e92cbdaddcf127be
Shiv Sakhuja <shivsak.s@gmail.com>
2023-11-03 03:59:37 +0530

    ATH-260: Wrapper around OpenAI completion to log to Athina automatically

---
+++ athina_logger/athina_meta.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class AthinaMeta:
+    prompt_slug: Optional[str] = None
+    context: Optional[dict] = None
+    customer_id: Optional[dict] = None
+    customer_user_id: Optional[dict] = None
+    session_id: Optional[dict] = None
+    user_query: Optional[dict] = None
+    environment: Optional[dict] = None
+    external_reference_id: Optional[dict] = None
+    customer_id: Optional[str] = None
+    customer_user_id: Optional[str] = None
+    response_time: Optional[int] = None
