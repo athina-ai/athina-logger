4554c539427b2cd50c21d07a96e1ed9fe78cec0c
Akshat Gupta <akshatwillrocku@gmail.com>
2024-02-02 20:20:03 +0530

    fix messages to prompt in token count helper

--- athina_logger/util/token_count_helper.py
+++ athina_logger/util/token_count_helper.py
@@ -35,7 +35,7 @@ def get_prompt_tokens_openai_chat_completion(prompt: List[Dict[str, Any]], langu
 
     try:
         num_tokens = 0
-        for message in messages:
+        for message in prompt:
             num_tokens += tokens_per_message
             for key, value in message.items():
                 num_tokens += len(encoding.encode(value))
