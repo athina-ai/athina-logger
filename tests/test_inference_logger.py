32ada1a12b60d55e0523d0bb5f93f408f4fa6ec2
Aleksandr Diamond <diamondaleksandr@gmail.com>
2024-10-28 23:35:42 -0700

    unit test for current sdk

--- tests/test_inference_logger.py
+++ tests/test_inference_logger.py
@@ -10,9 +10,9 @@ from athina_logger.inference_logger import InferenceLogger
     "language_model_id, prompt, response, user_query, context, prompt_tokens, "
     "completion_tokens, total_tokens, cost, response_time, prompt_slug, environment, "
     "customer_id, customer_user_id, session_id, expected_response, tools, tool_calls, "
-    "functions, function_call_response, external_reference_id, custom_attributes, model_options",
+    "functions, function_call_response, external_reference_id, custom_attributes",
     [
-        # first test case: includes all fields
+        # smoke test with all fields in the sdk param.
         (
                 "gpt-4",
                 [
@@ -45,50 +45,14 @@ from athina_logger.inference_logger import InferenceLogger
                 None,
                 "ref-101112",
                 {"company": "OpenAI", "links": ["https://openai.com"]},
-                {"temperature": 1, "max_completion_tokens": 10, "stop": ["apollo", "mars"], "top_p": 1}
         ),
-        # second test case: omitting model_options as an optional param
-        (
-                "gpt-4",
-                [
-                    {
-                        "role": "system",
-                        "content": "Answer the following question using the information provided.\n ### INFORMATION ### Neil Armstrong landed on the moon in 1969.\n ### QUERY ###"
-                    },
-                    {
-                        "role": "user",
-                        "content": "Which spaceship was first to land on the moon?"
-                    }
-                ],
-                "The Apollo 11 was the first spaceship to land on the moon.",
-                "Which spaceship was first to land on the moon?",
-                {"information": ["Neil Armstrong landed on the moon in 1969."]},
-                22,
-                9,
-                31,
-                0.002,
-                150,
-                "qa_chatbot_response",
-                "development",
-                "xyz-123",
-                "user-456",
-                "session-789",
-                "The Apollo 11 was the first spaceship to land on the moon.",
-                [],
-                None,
-                [],
-                None,
-                "ref-101112",
-                {"company": "OpenAI", "links": ["https://openai.com"]},
-                None  # `model_options` not included for optional param testing
-        )
     ]
 )
 def test_log_inference(
         language_model_id, prompt, response, user_query, context, prompt_tokens,
         completion_tokens, total_tokens, cost, response_time, prompt_slug, environment,
         customer_id, customer_user_id, session_id, expected_response, tools, tool_calls,
-        functions, function_call_response, external_reference_id, custom_attributes, model_options
+        functions, function_call_response, external_reference_id, custom_attributes
 ):
     athina_api_key = os.getenv("ATHINA_API_KEY")
 
@@ -117,5 +81,4 @@ def test_log_inference(
         function_call_response=function_call_response,
         external_reference_id=external_reference_id,
         custom_attributes=custom_attributes,
-        # model_options=model_options
     )
